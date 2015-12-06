b5fbbc3a66249d9e9712700ab33173ada6a81644
bapboy18 <bapboy18@yahoo.com.vn>
2015-12-06 23:21:37 +0700

    log in and log out user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+  end
+
   def show
     @user = User.find params[:id]
   end
@@ -10,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = "Create account successful!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root "static_pages#home"
 
-  get "static_pages/about"
   get "signup"  => "users#new"
+
+  get "static_pages/about"
+  get    "login"   => "sessions#new"
+  post   "login"   => "sessions#create"
+  delete "logout"  => "sessions#destroy"
   resources :users
 end
