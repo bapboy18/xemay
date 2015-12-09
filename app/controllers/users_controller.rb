bf3fcac54996b348709c77227fe609a5dddd367b
bapboy18 <duongnv.dhbk@gmail.com>
2015-12-09 13:06:58 +0700

    CRUD user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  def index
-  end
+  before_action :logged_in_user, only: [:show, :edit, :update]
 
   def show
     @user = User.find params[:id]
@@ -19,6 +18,20 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render "new"
+    end
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
