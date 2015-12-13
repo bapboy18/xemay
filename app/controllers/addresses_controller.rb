a25271bd0d4df076f8ba5e1438df505c2aaea2f0
bapboy18 <bapboy18@yahoo.com.vn>
2015-12-14 03:44:55 +0700

    fix several form

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,12 +3,12 @@ class AddressesController.rb < ApplicationController
   def create
     @review = Review.find params[:review_id]
     @address = @review.addresses.new address_params
-    # @address.region_id = @review.region_id
+    @address.region_id = @review.region_id
     @address.save
   end
 
   private
   def address_params
-    params.require(:address).permit :review_id, :name, :lat, :lng
+    params.require(:address).permit :region_id, :review_id, :name, :lat, :lng
   end
 end
