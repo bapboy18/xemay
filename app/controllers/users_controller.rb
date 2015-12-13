af10b759a725375ba747f317071a9b00299df707
bapboy18 <duongnv.dhbk@gmail.com>
2015-12-13 14:37:34 +0700

    remove images from old commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,10 +53,10 @@ class UsersController < ApplicationController
   end
 
   def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 end
